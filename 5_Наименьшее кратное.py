c7220e9036a85fc5b7c643d00d77bb875d4ea441
Hexqueller <49871848+V1RUSnya@users.noreply.github.com>
2023-11-20 10:10:38 +0300

    Update

--- 5_Наименьшее кратное.py
+++ 5_Наименьшее кратное.py
@@ -1,16 +1,8 @@
 #2520 - самое маленькое число, которое делится без остатка на все числа от 1 до 10.
 #Какое самое маленькое число делится нацело на все числа от 1 до 20?
-Number = 2
-Non_stop = True
-while Non_stop:
-    count = 1
-    for i in range (1,21):
-        var = Number % i
-        if var == 0:
-            count += 1
-    if count == 20:
-        print(f"Число найдено: {Number}")
-        Non_stop = False
-    else:
-        print(Number)
-        Number += 1
+from math import gcd
+num = 1
+for i in range(2, 21):
+    num = (num * i) // gcd(num, i)
+    print(num)
+print(f"{num} - наименьшее число")
