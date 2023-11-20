b5008c8d881a260ce72f883147b9baf45fb81061
Hexqueller <49871848+V1RUSnya@users.noreply.github.com>
2023-11-20 15:09:19 +0300

    Add files via upload

---
+++ 7_10001-е простое число.py
@@ -0,0 +1,19 @@
+#Выписав первые шесть простых чисел, получим 2, 3, 5, 7, 11 и 13. Очевидно, что 6-е простое число - 13.
+#Какое число является 10001-м простым числом?
+num = 2
+numbersCount = 0
+while True:
+    num +=1
+    count = 0
+    for i in range (num+1):
+        if i != 0:
+            if num%i == 0:
+                count += 1
+                if count > 3:
+                     break
+    if count == 2:
+            print(f"Найдено число {num}")
+            numbersCount +=1                
+    if numbersCount == 10001:
+        break            
+print(f"10001 число: {num}")
